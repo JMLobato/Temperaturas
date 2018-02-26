941c9a47d87b2a9e17b308eca67f41dbc82f06eb
JMLobato <derc8aid@gmail.com>
2018-02-26 18:00:08 +0100

    he modificado el fichero con extension .py

--- temperaturas.py
+++ temperaturas.py
@@ -2,6 +2,10 @@ from lxml import etree
 provincia=input("Introduce una comunidad de Sevilla: ")
 doc=etree.parse('sevilla.xml')
 raiz=doc.getroot()
-cod=raiz[0].attrib
-print(str(cod).split("-")[1])
+lista=raiz.findall("municipio")
+for elem in lista:
+	if (elem.text).upper() == provincia.upper():
+		cod=raiz.attrib
+		codigo=str(cod).split("-")[1].strip("id").strip("'}")
+		print('http://www.aemet.es/xml/municipios/localidad_%s.xml'%codigo)
 #doc=etree.parse('http://www.aemet.es/xml/municipios/localidad_%s.xml'%codigo)
