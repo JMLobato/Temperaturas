ebdc9c8cf3bbc4bcbbec9885f748f51d01360134
JMLobato <derc8aid@gmail.com>
2018-03-05 08:31:53 +0100

    Modificación del programa

--- temperaturas.py
+++ temperaturas.py
@@ -2,10 +2,11 @@ from lxml import etree
 provincia=input("Introduce una comunidad de Sevilla: ")
 doc=etree.parse('sevilla.xml')
 raiz=doc.getroot()
-lista=raiz.findall("municipio")
+lista=raiz.xpath("municipio")
 for elem in lista:
 	if (elem.text).upper() == provincia.upper():
-		cod=raiz.attrib
-		codigo=str(cod).split("-")[1].strip("id").strip("'}")
-		print('http://www.aemet.es/xml/municipios/localidad_%s.xml'%codigo)
+		cod=elem.attrib
+		#codigo=str(cod).split("-")[1].strip("id").strip("'}")
+		#print('http://www.aemet.es/xml/municipios/localidad_%s.xml'%codigo)
+		print(cod)
 #doc=etree.parse('http://www.aemet.es/xml/municipios/localidad_%s.xml'%codigo)
