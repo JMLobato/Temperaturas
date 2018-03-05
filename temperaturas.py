b44ac21700d0b395b1c56d1573d13c1809d56306
JMLobato <derc8aid@gmail.com>
2018-03-05 09:06:24 +0100

    Modificación del programa

--- temperaturas.py
+++ temperaturas.py
@@ -8,10 +8,14 @@ for elem in lista:
 	if (elem.text).upper() == provincia.upper():
 		cod=elem.attrib
 		codigo=cod["value"].split("-")[-1].strip("id")
-		#print('http://www.aemet.es/xml/municipios/localidad_%s.xml'%codigo)
+		print('http://www.aemet.es/xml/municipios/localidad_%s.xml'%codigo)
 doc=etree.parse('http://www.aemet.es/xml/municipios/localidad_%s.xml'%codigo)
 raiz=doc.getroot()
 today = date.today()
-dia=raiz.xpath("prediccion/dia")[1]
-print(dia.attrib)
-print(today)
+dia=raiz.xpath("prediccion/dia")[0].attrib["fecha"]
+
+if str(dia) == str(today):
+	print("La temperatura Max es de ",end="")
+	print(raiz.xpath("prediccion/dia")[0].xpath("temperatura/maxima")[0].text)
+	print("La temperatura Min es de ",end="")
+	print(raiz.xpath("prediccion/dia")[0].xpath("temperatura/minima")[0].text)
